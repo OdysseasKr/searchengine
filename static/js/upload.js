1e758f442074b474b4d1b5a1cc2280abc42ce5e6
OdysseasKr <OdysseasKr@users.noreply.github.com>
2018-01-31 18:41:03 +0200

    Prevent form submition by pressing enter

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,3 +1,10 @@
+$(window).keydown(function(event){
+	if(event.keyCode == 13) {
+		event.preventDefault();
+		return false;
+	}
+});
+
 $("#uploadbtn").click(function (e) {
 	$(".notice").text("");
 	var name = $("#namebox").val().trim();
